42c3d78e2f843042ef5f871f271c816a518aa41a
DeekshaHarish12 <157031205+DeekshaHarish12@users.noreply.github.com>
2026-04-15 21:49:51 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,16 +1,31 @@
 package com.example;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-public class App
-{
-    public static void main( String[] args )
-    {
-        WebDriver driver = new ChromeDriver();
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class App {
+    public static void main(String[] args) {
+
+        System.out.println("Starting Selenium Test...");
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
+        WebDriver driver = new ChromeDriver(options);
+
         driver.get("https://practicetestautomation.com/practice-test-login/");
         driver.manage().window().maximize();
+
         driver.findElement(By.id("username")).sendKeys("student");
         driver.findElement(By.id("password")).sendKeys("Password123");
         driver.findElement(By.id("submit")).click();
+
+        System.out.println("Test Completed!");
+
+        driver.quit();
     }
 }
